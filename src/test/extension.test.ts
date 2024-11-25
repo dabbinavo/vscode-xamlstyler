b3e428f483e1223a2998907da86a5d5d3548fbda
Christian Fischerauer <christian.fischerauer@gmail.com>
2024-11-25 20:37:05 +0100

    test: start writing first integration test

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,15 +1,32 @@
-import * as assert from 'assert';
+import * as assert from "assert";
+import * as path from "path";
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 // import * as myExtension from '../../extension';
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+// suite("Extension Test Suite", () => {
+//   vscode.window.showInformationMessage("Start all tests.");
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+//   test("Sample test", () => {
+//     assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+//     assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+//   });
+// });
+
+suite("Formatter Functionality Tests", () => {
+  vscode.window.showInformationMessage("Start formatter tests.");
+
+  const TEST_DATA_DIR = path.join(__dirname, "../../src/test/fixtures/FormatTests/");
+
+  test("Attribute Indentation", async () => {
+    
+    const directory = path.join(TEST_DATA_DIR, "AttributeFormatting");
+    const files = await vscode.workspace.findFiles(`${directory}/*.xaml`);
+
+    await vscode.workspace
+      .getConfiguration("xamlstyler.attributes.formatting")
+      .update("tolerance", 0, vscode.ConfigurationTarget.WorkspaceFolder);
+  });
 });
